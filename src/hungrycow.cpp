6bcad23fb27ad5b7286f3dfffce52639d9b90da6
tiffanygan <tiffanygan2015@gmail.com>
2023-08-15 17:03:23 -0700

    hungry cow update

--- src/hungrycow.cpp
+++ src/hungrycow.cpp
@@ -11,39 +11,41 @@ int main() {
 //    freopen(".in", "r", stdin);
 //    freopen(".out", "w", stdout);
 
-    int numDel;
+    int numDeliveries;
     ll numDays;
-    cin >> numDel >> numDays;
-    set<ll> delDays;
+    cin >> numDeliveries >> numDays;
     vector<pair<ll, int>> deliveries;
-    for (int i = 0; i < numDel; ++i) {
+    for (int i = 0; i < numDeliveries; ++i) {
         ll day;
         int amt;
         cin >> day >> amt;
-        pair<ll, int> temp = {day, amt};
-        deliveries.emplace_back(temp);
-        delDays.emplace(day);
+        deliveries.emplace_back(day, amt);
     }
 
+    // first delivery day
     ll numEaten = 1;
     ll numLeft = deliveries[0].second - 1;
-    for (int i = 1; i < numDel; ++i) {
-        ll numDaysPassed = deliveries[i].first - deliveries[i - 1].first - 1;
-        if (numLeft - numDaysPassed > 0) {
-            numEaten += numDaysPassed;
-            numLeft -= numDaysPassed;
-        } else if (numLeft > 0) {
+    ll prevDeliveryDay = deliveries[0].first;
+    ll currDeliveryDay;
+    for (int i = 1; i < numDeliveries; ++i) {
+        currDeliveryDay = deliveries[i].first;
+        ll daysInBetween = currDeliveryDay - prevDeliveryDay - 1;
+        if (numLeft > daysInBetween) {
+            numEaten += daysInBetween;
+            numLeft -= daysInBetween;
+        } else {
             numEaten += numLeft;
             numLeft = 0;
         }
         numLeft += deliveries[i].second;
         ++numEaten;
         --numLeft;
+        prevDeliveryDay = currDeliveryDay;
     }
 
-    if (deliveries[numDel - 1].first < numDays) {
-        if (numLeft - (numDays - deliveries[numDel - 1].first) > 0) {
-            numEaten += numDays - deliveries[numDel - 1].first;
+    if (deliveries[numDeliveries - 1].first < numDays) {
+        if (numLeft - (numDays - deliveries[numDeliveries - 1].first) > 0) {
+            numEaten += numDays - deliveries[numDeliveries - 1].first;
         } else if (numLeft > 0) {
             numEaten += numLeft;
         }

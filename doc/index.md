636d34c791a68dd02cb665fa492c3f90a50f2916
Tiffany Gan <tiffanygan2015@gmail.com>
2023-08-19 21:06:40 -0700

    Hungry Cow

--- doc/index.md
+++ doc/index.md
@@ -1,10 +1,12 @@
 # Bronze
 
-[Circular Barn](../src/circularbarn.cpp)
+* [Circular Barn](../src/circularbarn.cpp)
 
-[Speeding Ticket](../src/speedingticket.cpp):
-* Use two pointers, one for Bessie and one for limit information
-* While traversing all ordered landmarks, decide whether the pointer of Bessie or the pointer of limit or both need to proceed
-* Pay attention that `>` or `<` should be used instead of `>=`, `<=` in line 46, 47
-* Keep track of all the landmarks (either Bessie or limit has new information)
-* Use `struct` instead of `pair` to make code more readable (without performance penalty)
+* [Hungry Cow](../src/hungrycow.cpp)
+
+* [Speeding Ticket](../src/speedingticket.cpp):
+    * Use two pointers, one for Bessie and one for limit information
+    * While traversing all ordered landmarks, decide whether the pointer of Bessie or the pointer of limit or both need to proceed
+    * Pay attention that `>` or `<` should be used instead of `>=`, `<=` in line 46, 47
+    * Keep track of all the landmarks (either Bessie or limit has new information)
+    * Use `struct` instead of `pair` to make code more readable (without performance penalty)
---
+++ src/hungrycow.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+//    freopen(".in", "r", stdin);
+//    freopen(".out", "w", stdout);
+
+    int numDeliveries;
+    ll numDays;
+    cin >> numDeliveries >> numDays;
+    vector<pair<ll, int>> deliveries;
+    for (int i = 0; i < numDeliveries; ++i) {
+        ll day;
+        int amt;
+        cin >> day >> amt;
+        deliveries.emplace_back(day, amt);
+    }
+
+    // first delivery day
+    ll numEaten = 1;
+    ll numLeft = deliveries[0].second - 1;
+    ll prevDeliveryDay = deliveries[0].first;
+    ll currDeliveryDay;
+    for (int i = 1; i < numDeliveries; ++i) {
+        currDeliveryDay = deliveries[i].first;
+        ll daysInBetween = currDeliveryDay - prevDeliveryDay - 1;
+        if (numLeft > daysInBetween) {
+            numEaten += daysInBetween;
+            numLeft -= daysInBetween;
+        } else {
+            numEaten += numLeft;
+            numLeft = 0;
+        }
+        numLeft += deliveries[i].second;
+        ++numEaten;
+        --numLeft;
+        prevDeliveryDay = currDeliveryDay;
+    }
+
+    ll daysToEnd = numDays - prevDeliveryDay;
+    if (daysToEnd > 0) {
+        if (numLeft > daysToEnd) {
+            numEaten += daysToEnd;
+        } else {
+            numEaten += numLeft;
+        }
+    }
+
+    cout << numEaten;
+
+    return 0;
+}
